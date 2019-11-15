fa4e14cb052a4d1bc42949d25f2a2bd9357bb05e
sergio <sergy05.gm@gmail.com>
2019-11-15 13:58:07 +0100

    add proyect "Login"

---
+++ FirstJFX/src/application/BlurController.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.effect.GaussianBlur;
+// import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class BlurController {
+	
+	@FXML
+    // private ImageView imageView;
+	private Pane pane;
+
+
+	@FXML
+    public void setblur() {           
+             pane.setEffect(new GaussianBlur(20));
+
+    }
+	
+	@FXML
+    public void setFocus() {           
+             pane.setEffect(new GaussianBlur(0));
+
+    }
+	
+	@FXML
+	private void handleClose() {
+	
+	        System.exit(0);
+	}
+	
+}
---
+++ JavaFoenix-Login2/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
+			AnchorPane pane = loader.load();
+			Scene scene = new Scene(pane);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
