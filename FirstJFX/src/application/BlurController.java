64e8e20cd728e77ef01243bce4f88bc03d82cb34
sergio <sergy05.gm@gmail.com>
2019-11-22 09:54:57 +0100

    Método de abrir ventana en BlurController

--- FirstJFX/src/application/BlurController.java
+++ FirstJFX/src/application/BlurController.java
@@ -1,9 +1,13 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.effect.GaussianBlur;
+import javafx.scene.layout.AnchorPane;
 // import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class BlurController {
 	
@@ -11,23 +15,43 @@ public class BlurController {
     // private ImageView imageView;
 	private Pane pane;
 
-
+	
+	// DESHACER DEGRADADO DE IMAGEN
 	@FXML
     public void setblur() {           
              pane.setEffect(new GaussianBlur(20));
 
     }
 	
+	// DEGRADAR IMAGEN
 	@FXML
     public void setFocus() {           
              pane.setEffect(new GaussianBlur(0));
 
     }
 	
+	// PARA CERRAR VENTANA
 	@FXML
 	private void handleClose() {
 	
 	        System.exit(0);
+	}
+	
+	// PARA ABRIR UNA VENTANA NUEVA
+	@FXML
+	private void openStage() {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Forest.fxml"));
+			pane = (AnchorPane) loader.load();
+			Scene scene = new Scene(pane);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+			
+		} catch (Exception e) {
+			
+		}
 	}
 	
 }
